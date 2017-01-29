2221b02bf6ebf205b0fb75827e1b6cb0e0c723fd
Jon Liauw <jdliauw@gmail.com>
2017-01-29 18:28:21 -0500

    Updated list comprehension version to use 'xs' to be consistent with recursive. Added base case to Trent's recursive function

--- HW1/Add10List.hs
+++ HW1/Add10List.hs
@@ -1,8 +1,9 @@
--- Part A
+module Add10List where
+add10_list_rec :: [Integer] -> [Integer]
 add10_list_comp :: [Integer] -> [Integer]
-add10_list_comp a = [ x+10 | x <- a]
 
--- Part B
--- add10_list_rec :: [Integer] -> [Integer]
--- add10_list_rec [] = error "can't pass in empty list"
--- add10_list_rec a = head(a) + 10 : add10_list_rec tail a
+add10_list_comp xs = [ x+10 | x <- xs]
+
+add10_list_rec [] = [] 
+add10_list_rec (x:xs) = x+10 : (add10_list_rec xs)
+
