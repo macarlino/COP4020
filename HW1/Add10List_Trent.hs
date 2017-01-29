db837ba0f210b02ec0b1052a57d5c64cd6a1280b
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-01-29 15:51:25 -0500

    Create Add10List_Trent.hs

---
+++ HW1/Add10List_Trent.hs
@@ -0,0 +1,9 @@
+module Add10List where
+add10_list_rec :: [Integer] -> [Integer]
+add10_list_comp :: [Integer] -> [Integer]
+
+
+add10_list_comp xs = [x+10 | x <- xs]
+
+add10_list_rec [] = []
+add10_list_rec (x:xs) = x+10 : (add10_list_rec xs)
