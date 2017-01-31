e401a4ada5aef926463f5de24575ebee5c87275c
Jon Liauw <jdliauw@gmail.com>
2017-01-30 19:43:25 -0500

    THIS SHOULD WORK BABBY

--- HW1/DeleteNth.hs
+++ HW1/DeleteNth.hs
@@ -1,16 +1,6 @@
 module DeleteNth where
 deleteNth :: (Eq a) => Int -> a -> [a] -> [a]
 
--- just example of what is passed in to deleteNth
--- deleteNth n toDelete [list]
-
--- base case 1: if we are passed an empty list just return the empty list
--- if deleteNth _  _ [] = []
-
--- base case 2: if the count is 0, return the tail of the list (without the element a)
-deleteNth 0 toDelete (x:xs) = if (toDelete == x) then xs else x : (deleteNth 1 toDelete xs)
-deleteNth n toDelete (x:xs) = if (toDelete == x) then x : (deleteNth (n-1) toDelete xs) else x : (deleteNth (n-1) toDelete xs)
-
--- inductive step 1: decrement the count of the toDelete element if it matches (or don't if it doesn't) and pass recursive call to it
---if a == toDelete a : deleteNth n-1 toDelete tail(list)
---else a : deleteNth n toDelete tail(list)
+deleteNth _ _ [] = []
+deleteNth 1 toDelete (x:xs) = if (toDelete == x) then xs else x : (deleteNth 1 toDelete xs)
+deleteNth n toDelete (x:xs) = if (toDelete == x) then x : (deleteNth (n-1) toDelete xs) else x : (deleteNth (n) toDelete xs)
