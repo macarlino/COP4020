f8ff54299358eb1b64a8ca2fda705bf4cde23c82
Jon Liauw <jdliauw@gmail.com>
2017-01-30 19:46:20 -0500

    Add ()

--- HW1/Add10List.hs
+++ HW1/Add10List.hs
@@ -4,4 +4,4 @@ add10_list_comp :: [Integer] -> [Integer]
 
 add10_list_comp xs = [ x+10 | x <- xs]
 add10_list_rec [] = [] 
-add10_list_rec (x:xs) = x+10 : (add10_list_rec xs)
+add10_list_rec (x:xs) = (x+10) : (add10_list_rec xs)
