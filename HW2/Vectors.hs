8375a31a125df1bf07c214a1b531faac6e7ac61a
Jon Liauw <jdliauw@gmail.com>
2017-02-04 10:58:57 -0500

    Tested in ghci running main

--- HW2/Vectors.hs
+++ HW2/Vectors.hs
@@ -5,15 +5,19 @@ module Vectors where
 type Vector = [Double]
 scale :: Double -> Vector -> Vector
 add :: Vector -> Vector -> Vector
-dot :: Vector -> Vector -> Double
+-- dot :: Vector -> Vector -> Double
 
 -- scale returns the Vector with each coordinate multiplied by the Double
- -- put your solution here
+scale factor vector = [factor * v | v <- vector]
 
 -- add returns a Vector that is the pointwise sum of the two arguments
 -- The two arguments are assumed to have the same length.
- -- put your solution here
+add [] [] = []
+add (x:xs) (y:ys) = x+y : add xs ys
 
 -- dot returns the dot product of the Vector arguments
 -- The two arguments are assumed to have the same length.
  -- put your solution here
+dot [] [] = 0
+dot (x:xs) (y:ys) = x*y + dot xs ys
+
