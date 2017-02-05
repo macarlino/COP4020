06843141551f39cc6f9197c2bb2ee3353e0461c5
Jon Liauw <jdliauw@gmail.com>
2017-02-04 19:17:12 -0500

    First 2 parts of Question 3, tested manually in ghci

--- HW2/BinaryRelationOps.hs
+++ HW2/BinaryRelationOps.hs
@@ -3,5 +3,11 @@ import BinaryRelation
 
 project1 :: (BinaryRelation a b) -> [a]
 project2 :: (BinaryRelation a b) -> [b]
-select :: ((a,b) -> Bool) -> (BinaryRelation a b) -> (BinaryRelation a b)
-compose :: Eq b => (BinaryRelation a b) -> (BinaryRelation b c) -> (BinaryRelationa c)
+-- select :: ((a,b) -> Bool) -> (BinaryRelation a b) -> (BinaryRelation a b)
+-- compose :: Eq b => (BinaryRelation a b) -> (BinaryRelation b c) -> (BinaryRelationa c)
+
+project1 [] = []
+project1 ((a, b) : rest) = a : project1 rest
+
+project2 [] = []
+project2 ((a, b) : rest) = b : project2 rest
