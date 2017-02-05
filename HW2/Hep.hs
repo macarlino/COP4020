b157e54ff6960b4a665727558f44cefb61a185b2
Jon Liauw <jdliauw@gmail.com>
2017-02-05 01:29:41 -0500

    mohr progress

--- HW2/Hep.hs
+++ HW2/Hep.hs
@@ -10,18 +10,18 @@ hep (x:y:z:txt)
    | (x, y, z) == ("laughing", "out", "loud")       = "lol"  : hep txt
    | (x, y, z) == ("be", "right", "back")           = "brb"  : hep txt
    | (x, y, z) == ("by", "the", "way")              = "btw"  : hep txt
-   | x == "your"                                    = "ur"   : hep (txt)
-   | x == "you"                                     = "u"    : hep (txt)
-   | x == "boyfriend"                               = "bf"   : hep (txt)
-   | x == "girlfriend"                              = "gf"   : hep (txt)
-   | x == "great"                                   = "gr8"  : hep (txt)
-   | x == "are"                                     = "r"    : hep (txt)
-   | (x, y) == ("see", "you")                       = "cya"  : hep (txt)
-   | (x, y) == ("I", "will")                        = "I'll" : hep (txt)
-   | not (null (y:z:txt))                           = "heee" : hep (y:z:txt) 
---   | not (null (y:z))                               = x      : hep (y:z)
---   | not (null y)                                   = x      : hep (y)
-   | otherwise                                      = x      : [] 
+   | x == "your"                                    = "ur"   : hep (y:z:txt)
+   | x == "you"                                     = "u"    : hep (y:z:txt)
+   | x == "boyfriend"                               = "bf"   : hep (y:z:txt)
+   | x == "girlfriend"                              = "gf"   : hep (y:z:txt)
+   | x == "great"                                   = "gr8"  : hep (y:z:txt)
+   | x == "are"                                     = "r"    : hep (y:z:txt)
+   | (x, y) == ("see", "you")                       = "cya"  : hep (z:txt)
+   | (x, y) == ("I", "will")                        = "I'll" : hep (z:txt)
+   | not (null (y:z:txt))                           = x      : hep (y:z:txt) 
+--   | not (null (y:z))                             = x      : hep (y:z)
+--   | not (null y)                                 = x      : hep (y)
+   | otherwise                                      = x      : y : [] 
  
 
 -- ["you","know","I","will","see","you","soon"]  "==" ["u","know","I'll","cya","soon"]
