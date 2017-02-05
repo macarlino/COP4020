0b4bf231eacaf49d3460687539cfecee10b58f33
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-05 11:17:18 -0500

    Update ListMin, 0 failures!

--- HW2/ListMin.hs
+++ HW2/ListMin.hs
@@ -1,1 +1,9 @@
+module ListMin where
+
 listMin :: (Ord a) => [a] -> a
+
+listMin [] = error "The list must be non-empty"
+listMin (x:xs) = findMin x xs
+    where
+        findMin a [] = a
+        findMin a (y:ys) = if (a < y) then findMin a ys else findMin y ys
