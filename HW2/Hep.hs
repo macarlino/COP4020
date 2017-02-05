cce117b794a7891197cc465a6f74b2590f6281e0
Jon Liauw <jdliauw@gmail.com>
2017-02-05 01:16:47 -0500

    Hey at least it compiles

--- HW2/Hep.hs
+++ HW2/Hep.hs
@@ -5,25 +5,23 @@ hep [] = []
 -- hep ([]:[]:[]) = []
 -- hep ([]:[]:[]:[]) = []
 
--- hep (x:txt) 
---   | x == "your"                                    = "ur"   : hep (txt)
---   | x == "you"                                     = "u"    : hep (txt)
---   | x == "boyfriend"                               = "bf"   : hep (txt)
---   | x == "girlfriend"                              = "gf"   : hep (txt)
---   | x == "great"                                   = "gr8"  : hep (txt)
---   | x == "are"                                     = "r"    : hep (txt)
---   | otherwise                                      = x      : hep (txt)
-
-hep (x:y:txt)
+hep (x:y:z:txt)
+   | (x, y, z) == ("for", "your", "information")    = "fyi"  : hep txt
+   | (x, y, z) == ("laughing", "out", "loud")       = "lol"  : hep txt
+   | (x, y, z) == ("be", "right", "back")           = "brb"  : hep txt
+   | (x, y, z) == ("by", "the", "way")              = "btw"  : hep txt
+   | x == "your"                                    = "ur"   : hep (txt)
+   | x == "you"                                     = "u"    : hep (txt)
+   | x == "boyfriend"                               = "bf"   : hep (txt)
+   | x == "girlfriend"                              = "gf"   : hep (txt)
+   | x == "great"                                   = "gr8"  : hep (txt)
+   | x == "are"                                     = "r"    : hep (txt)
    | (x, y) == ("see", "you")                       = "cya"  : hep (txt)
    | (x, y) == ("I", "will")                        = "I'll" : hep (txt)
-   | otherwise                                      = if not (null y) then x : hep(y:txt) else []
-
--- hep (x:y:z:txt)
---   | (x, y, z) == ("for", "your", "information")    = "fyi"  : hep txt
---   | (x, y, z) == ("laughing", "out", "loud")       = "lol"  : hep txt
---   | (x, y, z) == ("be", "right", "back")           = "brb"  : hep txt
---   | (x, y, z) == ("by", "the", "way")              = "btw"  : hep txt
---   | otherwise                                      = x        : hep (y:z:txt)
+   | not (null (y:z:txt))                           = "heee" : hep (y:z:txt) 
+--   | not (null (y:z))                               = x      : hep (y:z)
+--   | not (null y)                                   = x      : hep (y)
+   | otherwise                                      = x      : [] 
+ 
 
 -- ["you","know","I","will","see","you","soon"]  "==" ["u","know","I'll","cya","soon"]
