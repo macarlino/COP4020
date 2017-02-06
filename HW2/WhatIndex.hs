9d8656145258be2929da736a17dd91734dd1fde6
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-02-06 00:46:45 -0500

    Works halfway
    Can return on empty list
    Can find the number, if its in the list.
    Can't count the index it was found in

--- HW2/WhatIndex.hs
+++ HW2/WhatIndex.hs
@@ -1,1 +1,11 @@
+-- using m to count the iterations
+m = 0
+
 whatIndex :: (Eq a) => a -> [a] -> Integer
+
+-- the the list is empty and/or the number wasn't in the list
+whatIndex _ [] = -1
+
+-- if the number was found, return the iteration
+-- otherwise use tail recursion
+whatIndex n (x:xs) = if (n == x) then m else whatIndex n xs
