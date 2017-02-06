d93480897791e32aac5e8bde098b1aab6a428046
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-06 11:46:58 -0500

    Update and completed WhatIndex.hs, 0 failures!

--- HW2/WhatIndex.hs
+++ HW2/WhatIndex.hs
@@ -1,11 +1,10 @@
--- using m to count the iterations
-m = 0
+module WhatIndex where
 
 whatIndex :: (Eq a) => a -> [a] -> Integer
 
--- the the list is empty and/or the number wasn't in the list
 whatIndex _ [] = -1
 
--- if the number was found, return the iteration
--- otherwise use tail recursion
-whatIndex n (x:xs) = if (n == x) then m else whatIndex n xs
+whatIndex n xs = findIndex 0 xs
+    where
+        findIndex _ [] = -1
+        findIndex c (x:xs) = if (n == x) then c else findIndex (c+1) xs
