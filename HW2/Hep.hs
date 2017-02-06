03c8a78d894585f0ffefba67722b7958f740a24b
Jon Liauw <jdliauw@gmail.com>
2017-02-06 17:21:23 -0500

    Passes all test cases but is definitely not optimal (code redundancy)
    It works, but is definitely not optimized.

--- HW2/Hep.hs
+++ HW2/Hep.hs
@@ -2,8 +2,6 @@ hep :: [String] -> [String]
 
 hep [] = []
 
--- hep (_:_:_:[]) = []
-
 hep (x:[])  
    | x == "your"                                    = "ur"   : [] 
    | x == "you"                                     = "u"    : []
@@ -16,13 +14,29 @@ hep (x:[])
 hep (x:y:[])
    | (x, y) == ("see", "you")                       = "cya"  : [] 
    | (x, y) == ("I", "will")                        = "I'll" : []
-   | x == "your"                                    = "ur"   : []
-   | x == "you"                                     = "u"    : []
-   | x == "boyfriend"                               = "bf"   : []
-   | x == "girlfriend"                              = "gf"   : []
-   | x == "great"                                   = "gr8"  : []
-   | x == "are"                                     = "r"    : []
-   | otherwise                                      = x      : []
+   | x == "your"                                    = "ur"   : hep (y:[]) 
+   | x == "you"                                     = "u"    : hep (y:[])
+   | x == "boyfriend"                               = "bf"   : hep (y:[])
+   | x == "girlfriend"                              = "gf"   : hep (y:[])
+   | x == "great"                                   = "gr8"  : hep (y:[])
+   | x == "are"                                     = "r"    : hep (y:[])
+   | otherwise                                      = x      : hep (y:[])
+
+hep (x:y:z:[])
+   | (x, y, z) == ("for", "your", "information")    = "t"  : []
+   | (x, y, z) == ("laughing", "out", "loud")       = "lol"  : []
+   | (x, y, z) == ("be", "right", "back")           = "brb"  : []
+   | (x, y, z) == ("by", "the", "way")              = "btw"  : []
+   | (x, y) == ("see", "you")                       = "cya"  : hep (z:[])
+   | (x, y) == ("I", "will")                        = "I'll" : hep (z:[])
+   | x == "your"                                    = "ur"   : hep (y:z:[])
+   | x == "you"                                     = "u"    : hep (y:z:[])
+   | x == "boyfriend"                               = "bf"   : hep (y:z:[])
+   | x == "girlfriend"                              = "gf"   : hep (y:z:[])
+   | x == "great"                                   = "gr8"  : hep (y:z:[])
+   | x == "are"                                     = "r"    : hep (y:z:[])
+   | otherwise                                      = x      : hep (y:z:[]) 
+ 
 
 hep (x:y:z:txt)
    | (x, y, z) == ("for", "your", "information")    = "fyi"  : hep txt 
@@ -45,21 +59,3 @@ hep (x:y:z:txt)
 -- hep ["by","the","way","you","must","see","my","girlfriend","she","is","great"]
 -- hep (["for","your","information","you","are","a","pig"] ++ ["see","you","later","when","you","find","me","a","boyfriend"]) 
 -- hep ["by","the","way","I","will","be","right","back"]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
