ef7dbe66ca91005fddf65a60151f439a759787a1
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-24 00:14:33 -0500

    Finished TotalHeight.hs - 0 failures!

--- HW3/TotalHeight.hs
+++ HW3/TotalHeight.hs
@@ -1,9 +1,22 @@
-module WindowLayout where
+module TotalHeight where
+import WindowLayout
+
+{-
 data WindowLayout = Window {wname :: String, width :: Int, height :: Int}
                   | Horizontal [WindowLayout]
                   | Vertical [WindowLayout]
-                  deriving (Show, Eq)
+                    deriving (Show, Eq) 
+-}
                   
 totalHeight :: WindowLayout -> Int
-totalHeight (Window _ _ h) = h
-totalHeight _ = 0
+totalHeight (Window {wname = nm, width = w, height = h}) = h
+-- horizontal is maximum of heights
+totalHeight (Horizontal wls) = totalMax wls
+-- vertical is sum of heights
+totalHeight (Vertical wls) = totalSum wls
+
+totalMax [] = 0
+totalMax (w:ws) = max (totalHeight w) (totalMax ws)
+
+totalSum [] = 0
+totalSum (w:ws) = totalHeight w + totalSum ws
