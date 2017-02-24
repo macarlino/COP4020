eafef39f77bde21fd32f94e4bb3dab9c9978f8c4
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-02-24 15:24:43 -0500

    Update FreeQBExp.hs

--- HW3/FreeQBExp.hs
+++ HW3/FreeQBExp.hs
@@ -8,8 +8,9 @@ freeQBExp (b `Or` c) = (freeQBExp b) ++ (freeQBExp c)
 freeQBExp (Not d) = (freeQBExp d) ++ []
 freeQBExp (Exists e f) = buildList e f
 
-concat' (Varref e) = []
-concat' (Exists f s) = buildList f s
-concat' (a `Or` b) = (freeQBExp a) ++ (freeQBExp b)
+buildList e f = [x | x <- (concat' f), x /= e]
 
-buildList f s = [x | x <- (concat' s), x /= f]
+concat' (Varref a) = [a]
+concat' (a `Or` b) = (freeQBExp a) ++ (freeQBExp b)
+concat' (Not d) = (freeQBExp d) ++ []
+concat' (Exists f s) = buildList f s
