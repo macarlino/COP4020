b417edfce8c449c47abb88f8ff6d3367ba95afc6
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-02-23 21:57:00 -0500

    Updated totalHeight Horizontal

--- HW3/TotalHeight.hs
+++ HW3/TotalHeight.hs
@@ -3,7 +3,30 @@ data WindowLayout = Window {wname :: String, width :: Int, height :: Int}
                   | Horizontal [WindowLayout]
                   | Vertical [WindowLayout]
                   deriving (Show, Eq)
-                  
+
+-- the height of a <WindowLayout> of form Window 
+-- {wname = nm, width = w, height = h} is h
+
 totalHeight :: WindowLayout -> Int
 totalHeight (Window _ _ h) = h
-totalHeight _ = 0
+
+-- the height of a <WindowLayout> of the form
+-- Horizontal [wl_1, .., wl_n] is 0 if the list if empty
+-- otherwise is the maximum of the height of wl_1 through wl_m (inclusive)
+
+totalHeight (Horizontal []) = 0
+--totalHeight (Horizontal xs) = [(Window wn w h) | (Window wn w h) <- xs]
+
+
+totalHeight ((Horizontal ((Window n w h):xs))) = findMax h xs
+    where 
+        findMax max [] = max
+        findMax max ((Window _ _ h):xs) = if max > h 
+                                          then findMax max xs 
+                                          else findMax h xs
+
+
+
+-- the height of a <WindowLayout> of the form
+-- Vertical [wl_1, .., wl_n] is the sum of the heights of wl_1 through wl_m
+-- which is 0 if the list is empty
