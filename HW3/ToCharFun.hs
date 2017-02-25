31c28bb93473a3787dacc553fe6332ff2cbd7dee
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-25 17:41:20 -0500

    Finish ToCharFun.hs - 0 failures!

--- HW3/ToCharFun.hs
+++ HW3/ToCharFun.hs
@@ -1,1 +1,8 @@
+module ToCharFun where
+
+-- higher-order functions
+    -- functions that take other functions as arguments or returns a function as result
+    
 toCharFun :: (Int -> Int) -> (Char -> Char)
+
+toCharFun f x = toEnum (f (fromEnum x))
