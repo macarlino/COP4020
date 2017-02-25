37a774c314dac68bcb9a33b2afb93d5eaf2be199
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-25 16:03:58 -0500

    Finished Rank.hs - 0 failures

--- HW3/Rank.hs
+++ HW3/Rank.hs
@@ -1,1 +1,12 @@
+module Rank where
+import Data.List
+
 rank :: (Ord a) => [a] -> [(Int, a)]
+
+rank [] = []
+rank xs = counter 1 1 (sort xs)
+
+counter _ _ [] = []
+counter p c (x:xs) = if (x == (if (xs /= []) then (head xs) else x)) 
+                    then ((c, x):(counter (p+1) c xs)) 
+                    else ((c, x):(counter 1 (c+p) xs))
