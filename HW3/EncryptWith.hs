dd535352b4b7b82f6d087160c2f23b9b68cccf35
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-26 18:57:13 -0500

    Finished EncryptWith.hs - 0 failures!

--- HW3/EncryptWith.hs
+++ HW3/EncryptWith.hs
@@ -1,1 +1,9 @@
+module EncryptWith where
+import ToCharFun
+
 encryptWith :: (Int -> Int) -> [String] -> [String]
+
+encryptWith _ [] = []
+encryptWith f (x:xs) = (character f x) : (encryptWith f xs)
+
+character f xs = [toCharFun f x | x <- xs]
