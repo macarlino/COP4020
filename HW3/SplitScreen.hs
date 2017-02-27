bb4e45257529d23b230b472269792412a9e3fbef
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-27 15:28:41 -0500

    Finished SplitScreen.hs - 0 failures!

--- HW3/SplitScreen.hs
+++ HW3/SplitScreen.hs
@@ -1,8 +1,12 @@
-module WindowLayout where
-data WindowLayout = Window {wname :: String, width :: Int, height :: Int}
-                  | Horizontal [WindowLayout]
-                  | Vertical [WindowLayout]
-                  deriving (Show, Eq)
+module SplitScreen where
+import WindowLayout
 
 splitScreen :: String -> WindowLayout -> WindowLayout
-splitScreen name (Window _ w h) = (Window name w h)
+
+splitScreen name (Window {wname = nm, width = w, height = h}) = if (nm == name) then Horizontal [(Window {wname = nm, width = w `div` 2, height = h}),(Window {wname = nm, width = w `div` 2, height = h})] else (Window {wname = nm, width = w, height = h})
+
+splitScreen name (Horizontal ws) = Horizontal (next name ws)
+splitScreen name (Vertical ws) = Vertical (next name ws)
+
+next _ [] = []
+next name (w:ws) = (splitScreen name w) : (next name ws)
