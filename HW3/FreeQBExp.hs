d7f1f056bc3d260bdf5e74966f4878b68cc9952b
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-27 12:47:45 -0500

    Finished FreeQBExp.hs - 0 failures!

--- HW3/FreeQBExp.hs
+++ HW3/FreeQBExp.hs
@@ -1,16 +1,20 @@
 module FreeQBExp where
 import QBExp
+import Data.List
+
+{-
+data QBExp = Varref String | QBExp `Or` QBExp 
+           | Not QBExp | Exists String QBExp
+             deriving (Eq, Show)
+-}
 
 freeQBExp :: QBExp -> [String]
 
-freeQBExp (Varref a) = [a]
-freeQBExp (b `Or` c) = (freeQBExp b) ++ (freeQBExp c)
-freeQBExp (Not d) = (freeQBExp d) ++ []
-freeQBExp (Exists e f) = buildList e f
+freeQBExp qbe = nub (helper qbe)
 
-buildList e f = [x | x <- (concat' f), x /= e]
+helper (Varref s) = [s]
+helper (e `Or` f) = (freeQBExp e) ++ (freeQBExp f)
+helper (Not e) = (freeQBExp e) ++ []
+helper (Exists s e) = buildList s e
 
-concat' (Varref a) = [a]
-concat' (b `Or` c) = (freeQBExp b) ++ (freeQBExp c)
-concat' (Not d) = (freeQBExp d) ++ []
-concat' (Exists e f) = buildList e f
+buildList s e = [x | x <- (freeQBExp e), x /= s]
