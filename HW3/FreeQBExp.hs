1d97a0c7fd632f57a34f0d781cf4ee59f7cc4d13
macarlino <carlino.matthew@knights.ucf.edu>
2017-02-27 12:55:48 -0500

    Finished FreeQBExp.hs - 0 failures!

--- HW3/FreeQBExp.hs
+++ HW3/FreeQBExp.hs
@@ -13,8 +13,8 @@ freeQBExp :: QBExp -> [String]
 freeQBExp qbe = nub (helper qbe)
 
 helper (Varref s) = [s]
-helper (e `Or` f) = (freeQBExp e) ++ (freeQBExp f)
-helper (Not e) = (freeQBExp e) ++ []
+helper (e `Or` f) = (helper e) ++ (helper f)
+helper (Not e) = (helper e) ++ []
 helper (Exists s e) = buildList s e
 
-buildList s e = [x | x <- (freeQBExp e), x /= s]
+buildList s e = [x | x <- (helper e), x /= s]
