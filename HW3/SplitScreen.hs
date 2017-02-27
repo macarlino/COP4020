a3a27020eb45391935e228c9cf2450934d63d2e8
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-02-27 15:33:43 -0500

    Update SplitScreen.hs

--- HW3/SplitScreen.hs
+++ HW3/SplitScreen.hs
@@ -3,7 +3,10 @@ import WindowLayout
 
 splitScreen :: String -> WindowLayout -> WindowLayout
 
-splitScreen name (Window {wname = nm, width = w, height = h}) = if (nm == name) then Horizontal [(Window {wname = nm, width = w `div` 2, height = h}),(Window {wname = nm, width = w `div` 2, height = h})] else (Window {wname = nm, width = w, height = h})
+splitScreen name (Window {wname = nm, width = w, height = h}) = 
+    if (nm == name) 
+    then Horizontal [(Window {wname = nm, width = w `div` 2, height = h}),(Window {wname = nm, width = w `div` 2, height = h})] 
+    else (Window {wname = nm, width = w, height = h})
 
 splitScreen name (Horizontal ws) = Horizontal (next name ws)
 splitScreen name (Vertical ws) = Vertical (next name ws)
