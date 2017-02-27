258b17a86f46e53b342640b828dd81faf78d521a
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-02-27 14:51:09 -0500

    Finished Improve.hs - 0 failures!

--- HW3/Improve.hs
+++ HW3/Improve.hs
@@ -1,15 +1,19 @@
 module Improve where
 import StatementsExpressions
 
-{- data Statement = ExpStmt Expression                
-               | AssignStmt String Expression
-               | IfStmt Expression Statement
-               deriving (Eq, Show) 
-
-data Expression = VarExp String
-                | NumExp Integer    
-                | EqualsExp Expression Expression
-                | BeginExp [Statement] Expression
-                deriving (Eq, Show) -}
-
 improve :: Statement -> Statement
+improve (ExpStmt e) = ExpStmt (expSimp e)
+improve (AssignStmt s e) = AssignStmt s (expSimp e)
+improve (IfStmt (VarExp "true") stmt) = improve stmt
+improve (IfStmt e stmt) = IfStmt (expSimp e) (improve stmt)
+ 
+expSimp :: Expression -> Expression
+expSimp (VarExp s) = (VarExp s)
+expSimp (NumExp n) = (NumExp n)
+expSimp (EqualsExp e1 e2) =
+    if bothVarExpp e1 e2
+    then VarExp "true"
+    else EqualsExp (expSimp e1) (expSimp e2)
+         where bothVarExpp _ _ = False
+expSimp (BeginExp [] e) = (expSimp e)
+expSimp (BeginExp stmt e) = (BeginExp(map improve stmt) (expSimp e))
