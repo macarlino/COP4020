fa61f8c5a9f96e882508a77a85a959fabb89ff8b
macarlino <carlino.matthew@knights.ucf.edu>
2017-03-28 21:47:55 -0400

    count_matching.erl finished - 0 failures!

--- HW5/count_matching.erl
+++ HW5/count_matching.erl
@@ -3,5 +3,18 @@
 
 -spec count_matching (fun((T) -> boolean()), list(T)) -> non_neg_integer(). 
 
+count_matching(_, []) -> 0;
 count_matching(Pred, Lst) ->
+    count_matching(Pred, Lst, 0). 
     
+%count_matching(Pred, [H|T], Counter) when Pred(H) -> 
+%    count_matching(Pred, T, Counter+1);
+
+count_matching(Pred, [H|T], Counter) ->
+    case Pred(H) of
+        true -> count_matching(Pred, T, Counter+1);
+        false -> count_matching(Pred, T, Counter)
+    end;
+    
+count_matching(_, [], Counter) ->
+    Counter. 
