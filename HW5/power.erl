ebc43a6af6651ed017a21235682f00d033a500eb
macarlino <carlino.matthew@knights.ucf.edu>
2017-03-29 20:51:04 -0400

    Did power.erl - hangs on testing

--- HW5/power.erl
+++ HW5/power.erl
@@ -1,5 +1,12 @@
 -module (power). 
 -export ([start/0]). 
 
+% compute_power(PS, N, M) ->                PS = start() from power.erl
+%    PS ! {self(), power, N, M},            self() is Pid ??    Pid is sender's process id
+%    receive {answer, Res} -> Res end.
+
 start() ->
-    
+    receive
+        {From, power, N, M} ->
+            From ! {self(), math:pow(N,M)};
+    end. 
