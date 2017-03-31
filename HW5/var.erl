2b1b93426c0b30219869102ae81e15f97762f6fb
macarlino <carlino.matthew@knights.ucf.edu>
2017-03-31 18:16:11 -0400

    var.erl partial skeleton

--- HW5/var.erl
+++ HW5/var.erl
@@ -1,5 +1,38 @@
 -module (var). 
 -export ([start/1]). 
 
+% B1 = var:start(1),
+% B2 = var:start(2),
+% [eqTest(vfetch(B1),"==",1)
+
+%vfetch(Pid) ->
+%    Pid!{self(), fetch},
+%    receive
+%	{value, Value} ->
+%	    Value
+%    after 3000 ->
+%	    io:format("timeout waiting for {value, Value} message~n"),
+%	    exit(wrong_message)
+%    end.
+
+%bassign(Pid, Value) ->
+%    Pid!{assign, Value},
+%    Value.
+
+
+
+
+% returns the Pid of a server
 start(Value) ->
-    
+    spawn(fun() -> loop(Value) end). 
+
+loop(Value) ->
+    receive
+        % makes the server continue with NewVal as its new value
+        {assign, NewVal} ->
+            
+        % Value is the server's current value
+        {Pid, fetch} ->
+            Pid ! {value, Value}
+    end. 
+        
