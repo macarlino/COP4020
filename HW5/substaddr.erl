6a0f27768b1e51f1c11f7aef2288e7f28567bf44
macarlino <carlino.matthew@knights.ucf.edu>
2017-04-05 17:50:40 -0400

    Update substadder.erl - almost finished

--- HW5/substaddr.erl
+++ HW5/substaddr.erl
@@ -1,39 +1,22 @@
 -module (substaddr). 
--export ([substaddr/3]). 
+-export ([substaddr/3, helper/3]). 
 -include ("salesdata.hrl"). 
 -import (salesdata, [store/2, group/2]). 
 
-% salesdata() is defined as either a store record or a group record:
-% -type salesdata() :: #store{} | #group{}.
-
-% Record definitions for the salesdata() type.
-%-record(store, {address :: string(), amounts :: [integer()]}).
-%-record(group, {gname :: string(), members :: [salesdata:salesdata()]}).
-
-
-% Description:
-% returns a sales data record that is just like SD except that all store records in SD whose address field's value is Old in SD are changed to New in the result.
-% Must Follow the Grammer! Use helping function for lists.
-
 -spec substaddr(salesdata:salesdata(), string(), string()) -> salesdata:salesdata(). 
 
-substaddr(SD, New, Old) ->
-    
-    
+% Need to go through the list and find a way to run substaddr to change address when needed
+helper(Members, New, Old) ->
+    [X || X <- Members]. 
 
+substaddr(#store{address=Address, amounts=Amounts}, New, Old) ->
+    if Address =:= Old ->
+        #store{address=New, amounts=Amounts};
+    true -> #store{address=Old, amounts=Amounts}
+    end; 
     
-    
-    
-% EXAMPLES:
-    
-%eqTest(substaddr(#group{gname = "StartUP!", members = []},"Downtown", "50 Washington Ave."),
-%	    "==", 
-%  #group{gname = "StartUP!", members = []})
-
-
-
-%eqTest(substaddr(
-%	      #group{gname = "Target", members = [#store{address = "The Mall", amounts = [10,32,55]},
-%                                               #store{address = "Downtown", amounts = [4,0,2,0]}]}, "253 Sears Tower", "The Mall"),
-%	    "==", 
-%  #group{gname = "Target", members = [#store{address = "253 Sears Tower", amounts = [10,32,55]}, #store{address = "Downtown", amounts = [4,0,2,0]}]})
+substaddr(#group{gname=Gname, members=Members}, New, Old) ->
+    if Members =:= [] ->
+        #group{gname = Gname, members = []};
+    true -> #group{gname = Gname, members = helper(Members, New, Old)}
+    end. 
