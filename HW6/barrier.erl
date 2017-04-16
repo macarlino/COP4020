48b35e7ca440f6aa0a8e6c07858dbca7b3d2bc06
Matthew Carlino <mcarlino@Matthew-Carlinos-MacBook.local>
2017-04-16 12:30:48 -0400

    Update barrier.erl - 1 fail

--- HW6/barrier.erl
+++ HW6/barrier.erl
@@ -2,7 +2,19 @@
 -export([start/1]). 
 
 start(X) ->
-	spawn(fun() -> server(X)). 
+	spawn(fun() -> server(X) end). 
 
 server(X) ->
+	receive
+		{Pid, done} ->
+			Pid ! {self(), ok},
+			if X =:= 1 -> 
+				Pid ! {self(), continue};
+				true -> ok  
+			end; 
+		{Pid, how_many_running} ->
+			Pid ! {self(), number_running_is, X},
+			server(X)
+	end,
+	server(X-1). 
 	
