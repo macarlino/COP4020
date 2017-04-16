ed9c55733f2b7afdcd147e949c1e25ebd79fd45c
tfreeman91 <tfreeman91@users.noreply.github.com>
2017-04-16 18:10:26 -0400

    eventdetector done - 0 failures

--- HW6/eventdetector.erl
+++ HW6/eventdetector.erl
@@ -1,11 +1,31 @@
--module(eventdetector). 
--export([start/2]). 
+-module(eventdetector).
+-export([start/2, loop/3]).
 
-start(InitialState, TransitionFun) ->
-	
+start(InitialState, TransitionFun) -> 
+	spawn(?MODULE, loop, [[], InitialState, TransitionFun]).
 
-	
+loop(Observers, State, Fun) ->
 	receive
 		{Pid, add_me} ->
-		Pid ! {added}
-	end. 
+			Pid ! {added},
+			loop([Pid | Observers], State, Fun);
+		{Pid, add_yourself_to, EDPid} ->
+			EDPid ! {self(), add_me},
+			receive
+				{added} ->
+					Pid ! {added},
+					loop(Observers, State, Fun)
+			end;
+		{Pid, state_value} ->
+			Pid ! {value_is, State},
+			loop(Observers, State, Fun);
+		Atom ->
+			{NewState, Event} = Fun(State, Atom),
+			if
+				Event =:= none ->
+					loop(Observers, NewState, Fun);
+				true ->
+					lists:foreach(fun(F) -> F ! Event end, Observers),
+					loop(Observers, NewState, Fun)
+			end
+	end.
